d42da568fea35453adb39c25a9e371908149fa0f
32ver <32ver@github.com>
2025-10-21 20:22:42 -0400

    fix: correct RecentUrls animation - slide down on entry, slide up on exit

--- spark-template/src/components/RecentUrls.tsx
+++ spark-template/src/components/RecentUrls.tsx
@@ -149,7 +149,7 @@ export function RecentUrls({ onUrlSelect }: RecentUrlsProps) {
 
   return (
     <motion.div
-      initial={{ opacity: 0, y: 20 }}
+      initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -20 }}
       transition={{ type: "spring", stiffness: 300, damping: 25 }}
