d36bdf6085da3d8e3f3d8d03f28759184df2351f
32ver <32ver@github.com>
2025-11-04 19:56:14 -0500

    Disable service worker ads for testing

--- spark-template/public/sw.js
+++ spark-template/public/sw.js
@@ -1,6 +1,2 @@
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10137490
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+// Service worker disabled for testing
+console.log('Service worker disabled');
